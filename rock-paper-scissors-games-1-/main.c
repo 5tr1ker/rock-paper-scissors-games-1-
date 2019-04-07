cb56cb9a2f31ec02697bb0e497b3ac364da506c2
Park S.J <follow2272@naver.com>
2019-04-07 23:27:54 +0900

    랜덤값 추가

--- rock-paper-scissors-games-1-/main.c
+++ rock-paper-scissors-games-1-/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int ran;
+int ham(void);
 int main()
 {
-	printf("welcome to C world");
+	ran = ham();
+	printf("컴퓨터가 무작위로 선택합니다... \n 1. 가위 2. 바위 3. 보 \n 컴퓨터가 (%d)를 선택했습니다",  ran );
 	return 0;
+}
+int ham(void)
+{
+	srand(time(NULL));
+	ran = rand() % ((3 - 1) + 1) + 1;
+	return ran;
 }
