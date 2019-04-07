f09c94582e1e039616da0914c8594fcfb9e4fc0f
Park S.J <follow2272@naver.com>
2019-04-07 23:48:08 +0900

    함수 이름 변경

--- rock-paper-scissors-games-1-/main.c
+++ rock-paper-scissors-games-1-/main.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 
 int ran;
-int ham(void);
+int ran_games_values(void);
 int main()
 {
-	ran = ham();
+	ran = ran_games_values();
 	printf("컴퓨터가 무작위로 선택합니다... \n 1. 가위 2. 바위 3. 보 \n 컴퓨터가 (%d)를 선택했습니다",  ran );
 	return 0;
 }
-int ham(void)
+int ran_games_values(void)
 {
 	srand(time(NULL));
 	ran = rand() % ((3 - 1) + 1) + 1;
