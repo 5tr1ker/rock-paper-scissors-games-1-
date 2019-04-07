5c8987aea1128971b0eb32f409c69a9e4851e879
Park S.J <follow2272@naver.com>
2019-04-08 00:15:34 +0900

    함수 간단화

--- rock-paper-scissors-games-1-/main.c
+++ rock-paper-scissors-games-1-/main.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 int ran;
-int ran_games_values(void);
+void ran_games_values();
 int main()
 {
 	srand(time(NULL));
-	ran = ran_games_values();
+	ran_games_values();
 	printf("컴퓨터가 무작위로 선택합니다... \n 0. 가위 1. 바위 2. 보 \n 컴퓨터가 (%d)를 선택했습니다",  ran );
 	return 0;
 }
-int ran_games_values(void)
+void ran_games_values()
 {
 	ran = rand() % 3;
-	return ran;
 }
