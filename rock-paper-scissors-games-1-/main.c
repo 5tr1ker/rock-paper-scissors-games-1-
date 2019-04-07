d995b13216c49559e0c1a5317d8315a3b4f8f05d
Park S.J <follow2272@naver.com>
2019-04-07 23:50:00 +0900

    처음 한번 시드값을 변경하도록 변경

--- rock-paper-scissors-games-1-/main.c
+++ rock-paper-scissors-games-1-/main.c
@@ -5,13 +5,13 @@ int ran;
 int ran_games_values(void);
 int main()
 {
+	srand(time(NULL));
 	ran = ran_games_values();
 	printf("컴퓨터가 무작위로 선택합니다... \n 1. 가위 2. 바위 3. 보 \n 컴퓨터가 (%d)를 선택했습니다",  ran );
 	return 0;
 }
 int ran_games_values(void)
 {
-	srand(time(NULL));
 	ran = rand() % ((3 - 1) + 1) + 1;
 	return ran;
 }
