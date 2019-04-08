a30a36f368e5260d1012026ace3d7906363d0e1d
Park S.J <follow2272@naver.com>
2019-04-08 22:32:53 +0900

    컴퓨터가 낸 값을 가리고 , 플레이어가 낸 값을 비교하여 결과를 알려주는 기능 추가

--- rock-paper-scissors-games-1-/main.c
+++ rock-paper-scissors-games-1-/main.c
@@ -1,16 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int ran;
+int ran, player;
 void ran_games_values();
+void game_result(int x);
 int main()
 {
 	srand(time(NULL));
 	ran_games_values();
-	printf("컴퓨터가 무작위로 선택합니다... \n 0. 가위 1. 바위 2. 보 \n 컴퓨터가 (%d)를 선택했습니다",  ran );
+	printf("컴퓨터가 무작위로 선택합니다... \n\n 0. 가위 1. 바위 2. 보 \n");
+
+	printf("플레이어가 낼 수를 입력해주세요 :");
+	scanf("%d", &player);
+	game_result(player);
 	return 0;
 }
 void ran_games_values()
 {
 	ran = rand() % 3;
+}
+void game_result(int x)
+{
+	if ((x == 0 && ran == 1) || (x == 1 && ran == 2) || (x == 2 && ran == 0))
+	{
+		printf("플레이어가 패배했습니다...");
+	}
+	else if (x == ran)
+	{
+		printf("비겼습니다...");
+	}
+	else
+		printf("플레이어가 이겼습니다...");
 }
